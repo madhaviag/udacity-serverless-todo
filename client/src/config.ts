e2d668f3d6732887623dedad13750af7b0f4d8fe
Madhavi Agnihotri <madhavi.agnihotri@gmail.com>
2022-01-19 18:20:32 +1100

    changes_to_update

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'dev-bq389e9j.us.auth0.com',           // Auth0 domain
   clientId: 'PFZKbbapO4hBRL4PJQM8zHCseV7L1kKi',  // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback' //callback
 }
